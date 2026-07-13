4f48de9b8acb2266d63b499108abfaf50bc98a41
kirill-daw <kirill-daw2002@yandex.ru>
2026-07-13 19:47:32 +0500

    fix: ensure image upload directory is created on startup

--- src/main/java/com/skypro/avito/service/ImageService.java
+++ src/main/java/com/skypro/avito/service/ImageService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @Service
@@ -18,12 +20,23 @@ public class ImageService {
         this.uploadPath = uploadPath;
     }
 
+    @PostConstruct
+    public void init() {
+        Path uploadDir = Paths.get(uploadPath);
+        if (!Files.exists(uploadDir)) {
+            try {
+                Files.createDirectories(uploadDir);
+            } catch (IOException e) {
+                throw new RuntimeException("Could not create upload directory: " + uploadPath, e);
+            }
+        }
+    }
+
     public String saveImage(MultipartFile image) {
         String extension = getExtension(image.getOriginalFilename());
         String filename = UUID.randomUUID() + "." + extension;
-        Path path = Path.of(uploadPath, filename);
+        Path path = Paths.get(uploadPath, filename);
         try {
-            Files.createDirectories(path.getParent());
             image.transferTo(path.toFile());
         } catch (IOException e) {
             throw new RuntimeException("Failed to save image", e);
