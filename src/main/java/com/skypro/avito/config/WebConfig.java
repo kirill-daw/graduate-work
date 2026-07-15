30a859817aae53dd57c7f16cc443416d408da21e
kirill-daw <kirill-daw2002@yandex.ru>
2026-07-15 15:49:03 +0500

    fix: resource handlers was deleted from WebConfig

--- src/main/java/com/skypro/avito/config/WebConfig.java
+++ src/main/java/com/skypro/avito/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.skypro.avito.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
@@ -23,11 +22,4 @@ public class WebConfig implements WebMvcConfigurer {
         usersDirUrl = Paths.get(uploadPath + "users/").toAbsolutePath().normalize().toUri().toString();
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/ads/**")
-                .addResourceLocations(adsDirUrl);
-        registry.addResourceHandler("/images/users/**")
-                .addResourceLocations(usersDirUrl);
-    }
 }
--- src/main/java/com/skypro/avito/controller/ImageController.java
+++ src/main/java/com/skypro/avito/controller/ImageController.java
@@ -3,7 +3,6 @@ package com.skypro.avito.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
